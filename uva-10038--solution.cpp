1a7182a2262de956f08fd190fbc5a78971cff443
MsNahid <mssnahid729@gmail.com>
2016-08-17 01:33:03 +0600

    Add files via upload

---
+++ uva-10038--solution.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <cstdlib>
+
+using namespace std;
+
+#define sz 3000
+
+int ara[sz + 5], dif[sz + 5];
+
+int main(){
+
+    int m, n, i, j;
+    int mx;
+
+    while(scanf("%d", &n) == 1){
+
+        j = 0;
+        mx = 0;
+        m = 0;
+
+        for(i = 0; i < n; i++) scanf("%d", &ara[i]);
+
+        for(i = 0; i < n - 1; i++){
+            dif[j++] = abs(ara[i] - ara[i + 1]);
+            if(mx < dif[i]){
+                mx = dif[i];
+            }
+        }
+        // start count sort
+        int cont[mx + 2];
+        for(i = 0; i <= mx + 1; i++) cont[i] = 0;
+
+        for(i = 0; i < j; i++){
+            cont[dif[i]]++;
+        }
+        dif[sz + 5] = {0};
+
+        for(i = 0; i <= mx; i++){
+            for(j = 1; j <= cont[i]; j++){
+                dif[m++] = i;
+            }
+        }
+        /* end count sort */
+
+        for(i = 0; i < m; i++){
+            if(dif[i] != i + 1){
+                puts("Not jolly");
+                break;
+            }
+        }
+
+        if(i == m) puts("Jolly");
+    }
+
+    return 0;
+}
